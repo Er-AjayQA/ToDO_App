4bac43a680aa5317a1a4f81d79ff231f176c6f77
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-05-17 16:51:00 +0530

    Changing the project structure

--- todo_admin_backend/api/UserManagement/Model/user.model.js
+++ todo_admin_backend/api/UserManagement/Model/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
       type: Array,
       ref: "project",
     },
-    activeProject: {
+    currentActiveProject: {
       type: String,
       ref: "project",
     },
