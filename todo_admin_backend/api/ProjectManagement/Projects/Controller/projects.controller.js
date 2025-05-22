bbbb5a6c2b7e8400579ac1cbcae0fb58d90cb772
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-05-22 13:12:33 +0530

    working on project update scenario

--- todo_admin_backend/api/ProjectManagement/Projects/Controller/projects.controller.js
+++ todo_admin_backend/api/ProjectManagement/Projects/Controller/projects.controller.js
@@ -51,20 +51,12 @@ exports.getAll = async (req, res) => {
     filter.name = nameRegex;
   }
 
-  if (
-    data.create_date !== null ||
-    data.create_date !== undefined ||
-    data.create_date !== ""
-  ) {
-    filter.create_date = req.body.create_date;
+  if (data?.create_date !== "" && data?.create_date !== undefined) {
+    filter.create_date = data?.create_date;
   }
 
-  if (
-    data.start_date !== null ||
-    data.start_date !== undefined ||
-    data.start_date !== ""
-  ) {
-    filter.start_date = req.body.start_date;
+  if (data?.start_date !== "" && data?.start_date !== undefined) {
+    filter.start_date = data?.start_date;
   }
 
   try {
@@ -83,6 +75,66 @@ exports.getAll = async (req, res) => {
         message: "Data fetched successfully!!",
         records: getAllProjects.length,
         data: getAllProjects,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong!!",
+      errorMessage: error.message,
+    });
+  }
+};
+
+// Update Project Details
+exports.update = async (req, res) => {
+  const data = req.body;
+  const filter = { isDeleted: false, company_id: req?.user?.companyId };
+
+  let updateData = {};
+
+  try {
+    if (limit < 1) limit = 10;
+
+    if (data?.name !== "" && data?.name !== undefined) {
+      updateData.name = data?.name;
+      filter.name = data?.name;
+    }
+    if (data?.description !== "" && data?.description !== undefined) {
+      updateData.description = data?.description;
+    }
+    if (data?.start_date !== "" && data?.start_date !== undefined) {
+      updateData.start_date = data?.start_date;
+    }
+    if (
+      data?.estimated_end_time !== "" &&
+      data?.estimated_end_time !== undefined
+    ) {
+      updateData.estimated_end_time = data?.estimated_end_time;
+    }
+
+    const isProjectExist = await ProjectModel.findOne(filter)
+      .limit(limit)
+      .sort({ _id: sort });
+
+    if (!isProjectExist) {
+      return res.status(201).json({
+        success: false,
+        message: "No Projects Found!!",
+      });
+    } else {
+      let updatedData = await ProjectModel.findByIdAndUpdate(
+        isProjectExist._id,
+        updateData,
+        {
+          new: true,
+        }
+      );
+
+      return res.status(201).json({
+        success: true,
+        message: "Data updated successfully!!",
+        data: updatedData,
       });
     }
   } catch (error) {
