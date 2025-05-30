a365690a930955ef2125e1f3c4330c3819fe4b54
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-05-30 12:38:54 +0530

    Creating the header and footer

--- todo_admin_client/tailwind.config.js
+++ todo_admin_client/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          primary: "#7E22CE",
+          hover: "#9333EA",
+          dark: "#6B21A8",
+          accent: "#A855F7",
+          light: "#D8B4FE",
+        },
+      },
+    },
   },
   plugins: [],
 };
