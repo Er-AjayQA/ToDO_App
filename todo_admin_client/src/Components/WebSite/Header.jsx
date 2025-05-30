7c529199f6ab0a175699c890793cb772d602b632
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-05-30 14:57:13 +0530

    Enhancing the header

--- todo_admin_client/src/Components/WebSite/Header.jsx
+++ todo_admin_client/src/Components/WebSite/Header.jsx
@@ -13,25 +13,25 @@ export const Header = () => {
     <>
       <header className="relative">
         {/* Logo Menu Start Here */}
-        <nav className="bg-white dark:bg-gray-900 sticky w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="sticky w-full z-20 top-0 start-0 bg-purple-dark border-b border-[#6B21A8] dark:border-[#6B21A8]">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-1">
             <Link
-              href="/home"
+              to="/home"
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
               <img
                 src="/Images/logo2.png"
                 className="h-12 w-auto md:h-16"
-                alt="Flowbite Logo"
+                alt="TaskTime Logo"
               />
-              <span className="hidden md:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              {/* <span className="hidden md:block self-center text-2xl text-white font-semibold whitespace-nowrap">
                 TickTock
-              </span>
+              </span> */}
             </Link>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse ">
               <button
                 type="button"
-                className="hidden md:block bg-purple-primary hover:bg-purple-hover text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+                className="hidden md:block bg-purple-accent hover:bg-purple-hover text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
               >
                 Register
               </button>
@@ -53,48 +53,48 @@ export const Header = () => {
         {/* Mobile Menu Start Here */}
         {isMobileMenuOpen ? (
           <div
-            className={`${
-              isMobileMenuOpen ? "block" : "hidden"
-            }  md:flex md:w-auto md:order-1 border border-gray-100 bg-gray-50`}
+            className={`absolute top-[56px] start-0 end-0 ${
+              isMobileMenuOpen ? "block" : ""
+            }  md:flex md:w-auto md:order-1 border border-gray-100 bg-[#D8B4FE]`}
           >
             <ul className="w-[50%] flex flex-col p-4 md:p-0 font-medium md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 mx-auto">
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                <Link
+                  to="#"
+                  className="block py-2 text-center px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                <Link
+                  to="#"
+                  className="block py-2 px-3 text-center text-gray-900 rounded-sm hover:text-purple-accent"
                 >
                   About
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                <Link
+                  to="#"
+                  className="block py-2 px-3 text-center text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Services
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                <Link
+                  to="#"
+                  className="block py-2 px-3 text-center text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
-                </a>
+                </Link>
               </li>
-              <li>
+              <li className="text-center">
                 <button
                   type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white bg-purple-primary hover:bg-purple-hover font-medium rounded-lg text-sm px-4 py-2 text-center mx-auto"
                 >
                   Register
                 </button>
@@ -108,12 +108,12 @@ export const Header = () => {
         {/* Mobile Menu End Here */}
 
         {/* Main Menu Start Here */}
-        <div className="hidden md:flex md:w-auto md:order-1">
+        <div className="hidden md:flex md:w-auto md:order-1 shadow-md shadow-purple-light">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 mx-auto">
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                className="block py-2 px-3 text-white bg-purple-accent rounded-sm hover:text-purple-accent"
                 aria-current="page"
               >
                 Home
@@ -122,7 +122,7 @@ export const Header = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:text-purple-accent"
               >
                 About
               </a>
@@ -130,7 +130,7 @@ export const Header = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded-sm  hover:text-purple-accent"
               >
                 Services
               </a>
@@ -138,7 +138,7 @@ export const Header = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded-sm  hover:text-purple-accent"
               >
                 Contact
               </a>
